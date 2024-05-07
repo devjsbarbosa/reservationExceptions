1d2c19f0ac92180d7edf92648dd4b1285acb53c4
João Barbosa <joao.sbarbosa@angulosistemas.com.br>
2024-05-07 12:46:22 -0300

    Implementação do toString

--- src/main/java/org/example/model/entities/Reservation.java
+++ src/main/java/org/example/model/entities/Reservation.java
@@ -1,16 +1,19 @@
 package org.example.model.entities;
 
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
 public class Reservation implements Serializable {
 
+    private static final SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
     private Integer roomNumber;
     private Date checkIn;
     private Date checkOut;
 
-    public Reservation () {}
+    public Reservation() {
+    }
 
     public Reservation(Integer roomNumber, Date checkin, Date checkout) {
         this.roomNumber = roomNumber;
@@ -21,30 +24,34 @@ public class Reservation implements Serializable {
     public Integer getRoomNumber() {
         return roomNumber;
     }
+
     public void setRoomNumber(Integer roomNumber) {
         this.roomNumber = roomNumber;
     }
+
     public Date getCheckIn() {
         return checkIn;
     }
+
     public Date getCheckOut() {
         return checkOut;
     }
 
 
-    public long duration(){
+    public long duration() {
         // pega a diferença nas datas em milisegundos
         long diff = checkOut.getTime() - checkIn.getTime();
         // converter o milisegundos em dias
         return TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS);
     }
 
-    public void updateDates(Date checkin, Date checkout){
-
+    public void updateDates(Date checkIn, Date checkOut) {
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
     }
 
     @Override
     public String toString() {
-        return "Reserva: Quarto " + roomNumber + ", check-in: " + checkIn + ", check-out: " + checkOut + duration();
+        return "Reserva: Quarto " + roomNumber + ", check-in: " + simpleDateFormat.format(checkIn) + ", check-out: " + simpleDateFormat.format(checkOut) + duration() + " noites";
     }
 }
